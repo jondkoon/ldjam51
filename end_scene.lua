6110ec6a717c079c2eea2ffde752a0794b2de226
Jon Koon <jondkoon@gmail.com>
2022-10-03 13:02:32 -0400

    Add end scene

---
+++ end_scene.lua
@@ -0,0 +1,27 @@
+end_scene = make_scene({
+  init = function(self)
+    self.start_game_text_blink = 0
+  end,
+  update = function(self)
+    self.start_game_text_blink += 1
+    if self.start_game_text_blink > 60 then self.start_game_text_blink = 1 end
+    if btnp(4) or btnp(5) then
+      change_scene(game_scene)
+    end
+  end,
+  draw = function(self)
+    cls(1)
+
+    palt(0, false)
+
+    spr(194, 48, 16, 4, 4)
+
+    center_print("wave: "..self.wave, 52, 7)
+    center_print("gold: "..self.gold, 60, 7)
+
+    -- instructions
+    if self.start_game_text_blink > 30 then
+      center_print("press z or x to try again", 100, 7)
+    end
+  end
+})
--- game_scene.lua
+++ game_scene.lua
@@ -29,6 +29,7 @@ end
 
 function make_turret(o)
   local turret = {
+    type = 'turret',
     color = o.color,
     scene = o.scene,
     width = 8,
@@ -38,13 +39,15 @@ function make_turret(o)
     flip_x = false,
     flip_y = true,
     fire_interval = 30,
+    range = 30,
+    debug_show_range = false,
     find_target = function(self)
       local closest
       local closest_mag
       for prince in all(self.scene.princes) do
         local diff = prince.pos - self.pos
         local mag = #diff
-        if (closest == nil or mag < closest_mag) then
+        if ((closest == nil or mag < closest_mag) and mag < self.range) then
           closest = prince
           closest_mag = mag
         end
@@ -87,7 +90,14 @@ function make_turret(o)
       self.flip_x = rounded.x == -1
       self.flip_y = rounded.y == 1      
     end,
+    draw_range = function(self)
+      local center = get_center(self)
+      circ(center.x, center.y, self.range, 5)
+    end,
     draw = function(self)
+      if (self.debug_show_range) then
+        self:draw_range()
+      end
       spr(self.sprite, self.pos.x, self.pos.y, 1, 1, self.flip_x, self.flip_y)
     end
   }
@@ -101,13 +111,13 @@ function make_prince(o)
     dp = vector{0,0},
     width = 2,
     height = 7,
-    speed = 0.75,
-    health = 10,
+    speed = 0.6,
+    health = 5,
     hit = function(self, power)
       self.health -= power
     end,
     complete = function(self)
-      self.scene:remove_prince(self)
+      self.scene:lost()
     end,
     died = function(self)
       self.scene:remove_prince(self)
@@ -161,7 +171,21 @@ function make_princess(o)
       elseif (math_round(self.dp.x) != 0) then
         sprite = 20
       end
+
+      -- draw cursor
+      if (self.cursor_pos) then
+        spr(11, self.cursor_pos.x, self.cursor_pos.y)
+      end
+
+      if (self.tile_info and self.tile_info.type == 'turret') then
+        self.tile_info:draw_range()
+      end
+
+      -- draw character
       spr(sprite,self.pos.x - 3, self.pos.y - 2,1,1, flip_x)
+    end,
+    place_turret = function(self)
+      self.scene:add_turret(self.cursor_pos)
     end,
     update = function(self)
       if (btn(0)) then
@@ -184,6 +208,20 @@ function make_princess(o)
 
       if (next_pos.x > 1 and next_pos.x < screen_width - 2 and next_pos.y > 8 and next_pos.y < screen_height - self.height) then
         self.pos = next_pos
+      end 
+
+      local tile_pos = from_tile_coordinate(to_tile_coordinate(self.pos - vector{3,2}))
+
+      self.tile_info = self.scene:get_tile_info(tile_pos)
+
+      if self.tile_info then
+        self.cursor_pos = tile_pos
+        if (btnp(4)) then
+          self:place_turret()
+        end
+      else
+        self.tile_info = nil
+        self.cursor_pos = nil
       end
     end
   }
@@ -203,7 +241,38 @@ function get_tile_key(tile_pos)
   return tile_pos.x..','..tile_pos.y
 end
 
+local is_turret_eligible = {
+  [2] = true,
+  [3] = true,
+  [6] = true
+}
+
+local wave_prince_count = {
+  1,
+  2,
+  4,
+  6,
+  10
+}
+
 game_scene = make_scene({
+  init_turret_grid = function(self)
+    local turret_grid = {}
+    local max_x = screen_width / 8
+    local max_y = screen_height / 8
+    for x = 0, max_x do
+      for y = 0, max_y do
+        local tile_n = mget(x, y)
+        if (is_turret_eligible[tile_n]) then
+          if (turret_grid[x] == nil) then
+            turret_grid[x] = {}
+          end
+          turret_grid[x][y] = false
+        end
+      end
+    end
+    self.turret_grid = turret_grid
+  end,
   init_path = function(self)
     local visited = {}
     self.path = {}
@@ -240,6 +309,11 @@ game_scene = make_scene({
     local next_pos = from_tile_coordinate(next_tile) + vector{3,0} -- convert to screen coordinates and move to center of tile
     return next_pos - pos
   end,
+  lost = function(self)
+    end_scene.gold = self.gold_earned
+    end_scene.wave = self.wave
+    change_scene(end_scene)
+  end,
   add_prince = function(self)
     local prince_start = vector{ self.start_tile_pos.x * 8 + 3, self.start_tile_pos.y * 8 }
     local prince = make_prince({ pos = prince_start, scene = self })
@@ -247,32 +321,54 @@ game_scene = make_scene({
     self:add(prince)
   end,
   remove_prince = function(self, prince)
+    self:add_gold(10)
     self:remove(prince)
     del(self.princes, prince)
   end,
+  get_tile_info = function(self, pos)
+    local tile_pos = to_tile_coordinate(pos)
+    local grid_x = self.turret_grid[tile_pos.x]
+    local tile_info = grid_x and grid_x[tile_pos.y]
+    if (tile_info == false) then
+      return { type = 'available' }
+    end
+    return tile_info
+  end,
   add_turret = function(self, pos)
-    local turret = make_turret({ color = 11, scene = self, pos = pos })
-    add(self.turrets, turret)
-    self:add(turret)
+    local turret_cost = 50
+    local tile_pos = to_tile_coordinate(pos)
+    local grid_x = self.turret_grid[tile_pos.x]
+    local is_eligible = grid_x and grid_x[tile_pos.y] == false
+    if (is_eligible and self.gold >= turret_cost) then
+      local turret = make_turret({ color = 11, scene = self, pos = pos })
+      grid_x[tile_pos.y] = turret
+      add(self.turrets, turret)
+      self:add(turret)
+      self.gold -= turret_cost
+    end
+  end,
+  add_gold = function(self, amount) 
+    self.gold += amount
+    self.gold_earned += amount
   end,
   init = function(self)
-    self.wave = 1
-    self.countdown = 2
+    self.prince_wave_count = 0
+    self.next_prince_wave_count = wave_prince_count[1]
+    self.wave = 0
+    self.wave_countdown = 10
 
     self.gold = 100
+    self.gold_earned = self.gold
 
     self.start_tile_pos = find_map_tile(start_tile,0,0,16,16)
     self.end_tile_pos = find_map_tile(end_tile,0,0,16,16)
 
     self:init_path()
+    self:init_turret_grid()
 
     self.turrets = {}
-    self:add_turret(vector{32,64})
-    self:add_turret(vector{64,64})
-    self:add_turret(vector{96,64})
 
     self.princes = {}
-    self:add_prince()
 
     self.princess = make_princess({ scene = self, pos = vector{ 30, 24 } })
     self:add(self.princess)
@@ -284,16 +380,22 @@ game_scene = make_scene({
     menuitem(1, "restart", function() change_scene(game_scene) end)
   end,
   next_wave = function(self)
-    self.countdown = 10
+    self.wave_countdown = 10
     self.wave += 1
-    self:add_prince()
+    self.prince_wave_count = wave_prince_count[self.wave] or 20
+    self.next_prince_wave_count = wave_prince_count[self.wave + 1] or 20
   end,
   update = function(self)
-    if (frame + 1 % 60 == 1) then
-      self.countdown -= 1
+    if (frame % 30 == 0 and self.prince_wave_count > 0) then
+      self.prince_wave_count -= 1
+      self:add_prince()
     end
 
-    if (self.countdown == -1) then
+    if (frame % 60 == 0) then
+      self.wave_countdown -= 1
+    end
+
+    if (self.wave_countdown == -1) then
       self:next_wave()
     end
 
@@ -331,7 +433,6 @@ game_scene = make_scene({
   after_draw = function(self)
     self.iris:draw()
     
-
     -- black HUD
     rectfill(0,0,screen_width, 6, 1)
 
@@ -342,12 +443,12 @@ game_scene = make_scene({
     -- wave indicator
     print("wave "..self.wave, 50, 1, 7)
 
-    -- countdown
+    -- wave_countdown
+    local next_count_x_offset = self.next_prince_wave_count >= 10 and 5 or 0
+    print(self.next_prince_wave_count, screen_width - 19 - next_count_x_offset, 1)
     spr(24, screen_width - 15,0)
-    local countdown_text = self.countdown == 10 and "10" or "0"..self.countdown
-    local countdown_color = self.countdown <= 3 and 8 or 7
+    local countdown_text = self.wave_countdown == 10 and "10" or "0"..self.wave_countdown
+    local countdown_color = self.wave_countdown <= 3 and 8 or 7
     print(countdown_text, screen_width - 8, 1, countdown_color)
-
-    
   end
 })
